4920f9876afa70f5fe6698f4ba6ce54180b64904
sabrinagalesi <sabrinagalesi8@gmail.com>
2020-06-03 17:20:51 +0200

    Iniziato grafica della pagina

---
+++ js/text.js
@@ -0,0 +1,3 @@
+/* MILESTONE 1 
+    -Replica della grafica ​ con la possibilità di avere messaggi scritti dall’utente (verdi) e dall’interlocutore (bianco) assegnando due classi CSS diverse 
+    -Aggiunta di un messaggio​ : l’utente scrive un testo nella parte bassa e cliccando “invia” il testo viene aggiunto al thread sopra, come messaggio verde */

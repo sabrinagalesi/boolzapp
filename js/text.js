07ec37c9f0c1e935047005b32af1f08a810b4a78
sabrinagalesi <sabrinagalesi8@gmail.com>
2020-06-16 12:29:57 +0200

    Implementato alcune funzioni

--- js/text.js
+++ js/text.js
@@ -1,3 +1,22 @@
 /* MILESTONE 1 
     -Replica della grafica ​ con la possibilità di avere messaggi scritti dall’utente (verdi) e dall’interlocutore (bianco) assegnando due classi CSS diverse 
-    -Aggiunta di un messaggio​ : l’utente scrive un testo nella parte bassa e cliccando “invia” il testo viene aggiunto al thread sopra, come messaggio verde */
+    -Aggiunta di un messaggio​ : l’utente scrive un testo nella parte bassa e cliccando “invia” il testo viene aggiunto al thread sopra, come messaggio verde 
+   MILESTONE 2 
+    -Risposta dall’interlocutore: ​ ad ogni inserimento di un messaggio, l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo. 
+    -Ricerca utenti: ​ scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i contatti 
+    il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo “mar” rimangono solo Marco e Martina)
+    */
+    
+    function rispostaInterlocutore() {
+        $("#messages").append("<div class='friend-messages-contenitor'><div class='friend-messages'><span class='messages-written'>ok</span></div></div>")
+    }
+
+    $(document).ready(function(){
+        $("#arrow").click(function(){
+            $("#messages").append('<div class="my-messages-contenitor"><div class="my-messages"><span class="messages-written">' + $("#write").val() + '</span></div></div>');
+            $("#write").val("");
+            setTimeout(rispostaInterlocutore,1000);
+        });
+    })
+
+    //Probabilmente per la ricerca devo usare .filter

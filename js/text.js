86d257f97b905d748bf72a6cbe8f248bd7d58161
sabrinagalesi <sabrinagalesi8@gmail.com>
2020-07-01 13:15:31 +0200

    finito!

--- js/text.js
+++ js/text.js
@@ -9,7 +9,13 @@
     -Click sul contatto​ mostra la conversazione del contatto cliccato, è possibile inserire nuovi messaggi per ogni conversazione 
     - Cancella messaggio: ​ cliccando sul messaggio appare un menu a tendina che permette di cancellare il messaggio selezionato.
     */
-    
+    function generaMessaggio(messaggio) {
+        return "<div class='friend-messages-contenitor'><div class='friend-messages'><img class='dropdown' src='./img/arrow.webp'/><div class='delete-msg' style='display: none;'>Elimina messaggio</div><span class='messages-written'>" + 
+            messaggio + 
+        "</span></div></div>";
+    }
+
+
     function rispostaInterlocutore() {
         var numeroRisposta = Math.floor(Math.random () * 2) + 1;
         console.log(numeroRisposta);
@@ -18,112 +24,113 @@
         if(numeroChat == 1){
             switch(numeroRisposta){
                 case 1:
-                    $(".messages:visible").append("<div class='friend-messages-contenitor'><div class='friend-messages'><img class='dropdown' src='./img/arrow.webp'/><div class='delete-msg' style='display: none;'>Elimina messaggio</div><span class='messages-written'>Wherever you go, I'll be with you <3 </span></div></div>");
+                    $(".messages:visible").append(generaMessaggio("Wherever you go, I'll be with you <3"));
                     break;
                 case 2:
-                    $(".messages:visible").append("<div class='friend-messages-contenitor'><div class='friend-messages'><img class='dropdown' src='./img/arrow.webp'/><div class='delete-msg' style='display: none;'>Elimina messaggio</div><span class='messages-written'> Ci sono tanti mondi, ma tutti condividono lo stesso cielo. Un solo cielo, un solo destino. </span></div></div>");
+                    $(".messages:visible").append(generaMessaggio("Ci sono tanti mondi, ma tutti condividono lo stesso cielo. Un solo cielo, un solo destino."));
                     break;
             } 
         } else if(numeroChat == 2){
             switch(numeroRisposta){
                 case 1:
-                    $(".messages:visible").append("<div class='friend-messages-contenitor'><div class='friend-messages'><img class='dropdown' src='./img/arrow.webp'/><div class='delete-msg' style='display: none;'>Elimina messaggio</div><span class='messages-written'>Turn to DARKNESS!!!! </span></div></div>");
+                    $(".messages:visible").append(generaMessaggio("Turn to DARKNESS!!!!"));
                     break;
                 case 2:
-                    $(".messages:visible").append("<div class='friend-messages-contenitor'><div class='friend-messages'><img class='dropdown' src='./img/arrow.webp'/><div class='delete-msg' style='display: none;'>Elimina messaggio</div><span class='messages-written'>Sarò io a mangiare la papaya insieme a Kairi, non Sora <3 :D</span></div></div>");
+                    $(".messages:visible").append(generaMessaggio("Sarò io a mangiare la papaya insieme a Kairi, non Sora <3 :D"));
                     break;
             }
         } else if (numeroChat == 3) {
             switch(numeroRisposta){
                 case 1:
-                    $(".messages:visible").append("<div class='friend-messages-contenitor'><div class='friend-messages'><img class='dropdown' src='./img/arrow.webp'/><div class='delete-msg' style='display: none;'>Elimina messaggio</div><span class='messages-written'>Ohohohohohohohohoh!!</span></div></div>");
+                    $(".messages:visible").append(generaMessaggio("Ohohohohohohohohoh!!"));
                     break;
                 case 2:
-                    $(".messages:visible").append("<div class='friend-messages-contenitor'><div class='friend-messages'><img class='dropdown' src='./img/arrow.webp'/><div class='delete-msg' style='display: none;'>Elimina messaggio</div><span class='messages-written'>All men are my servants!!</span></div></div>");
+                    $(".messages:visible").append(generaMessaggio("All men are my servants!!"));
                     break;
             } 
         } else if (numeroChat == 4) {
             switch(numeroRisposta){
                 case 1:
-                    $(".messages:visible").append("<div class='friend-messages-contenitor'><div class='friend-messages'><img class='dropdown' src='./img/arrow.webp'/><div class='delete-msg' style='display: none;'>Elimina messaggio</div><span class='messages-written'>Let's super go!</span></div></div>");
+                    $(".messages:visible").append(generaMessaggio("Let's super go!"));
                     break;
                 case 2: 
-                    $(".messages:visible").append("<div class='friend-messages-contenitor'><div class='friend-messages'><img class='dropdown' src='./img/arrow.webp'/><div class='delete-msg' style='display: none;'>Elimina messaggio</div><span class='messages-written'>Supeeeeeer......UNIVEEEEERSEEEEEE!!!! </span></div></div>");
+                    $(".messages:visible").append(generaMessaggio("Supeeeeeer......UNIVEEEEERSEEEEEE!!!!"));
                     break;
             }
         } else if (numeroChat == 5) {
             switch(numeroRisposta){
                 case 1:
-                    $(".messages:visible").append("<div class='friend-messages-contenitor'><div class='friend-messages'><img class='dropdown' src='./img/arrow.webp'/><div class='delete-msg' style='display: none;'>Elimina messaggio</div><span class='messages-written'>I'll destroy all of you!</span></div></div>");
+                    $(".messages:visible").append(generaMessaggio("I'll destroy all of you!"));
                     break;
                 case 2:
-                    $(".messages:visible").append("<div class='friend-messages-contenitor'><div class='friend-messages'><img class='dropdown' src='./img/arrow.webp'/><div class='delete-msg' style='display: none;'>Elimina messaggio</div><span class='messages-written'> Hahahahahahahaha!!!! </span></div></div>");
+                    $(".messages:visible").append(generaMessaggio("Hahahahahahahaha!!!!"));
                     break;
             }
         } else if (numeroChat == 6) {
             switch(numeroRisposta) {
                 case 1:
-                    $(".messages:visible").append("<div class='friend-messages-contenitor'><div class='friend-messages'><img class='dropdown' src='./img/arrow.webp'/><div class='delete-msg' style='display: none;'>Elimina messaggio</div><span class='messages-written'>The power of LOOOVEEEE! </span></div></div>");
+                    $(".messages:visible").append(generaMessaggio("The power of LOOOVEEEE!"));
                     break;
                 case 2:
-                    $(".messages:visible").append("<div class='friend-messages-contenitor'><div class='friend-messages'><img class='dropdown' src='./img/arrow.webp'/><div class='delete-msg' style='display: none;'>Elimina messaggio</div><span class='messages-written'>Are you okay? </span></div></div>");
+                    $(".messages:visible").append(generaMessaggio("Are you okay?"));
                     break;
             }
         } else if(numeroChat == 7) {
             switch(numeroRisposta) {
                 case 1:
-                    $(".messages:visible").append("<div class='friend-messages-contenitor'><div class='friend-messages'><img class='dropdown' src='./img/arrow.webp'/><div class='delete-msg' style='display: none;'>Elimina messaggio</div><span class='messages-written'>Where would you like to go, dude?</span></div></div>");
+                    $(".messages:visible").append(generaMessaggio("Where would you like to go, dude?"));
                     break;
                 case 2:
-                    $(".messages:visible").append("<div class='friend-messages-contenitor'><div class='friend-messages'><img class='dropdown' src='./img/arrow.webp'/><div class='delete-msg' style='display: none;'>Elimina messaggio</div><span class='messages-written'>DUDE!</span></div></div>");
+                    $(".messages:visible").append(generaMessaggio("DUDE!"));
                     break;
             }
         } else if(numeroChat == 8){
             switch(numeroRisposta) {
                 case 1:
-                    $(".messages:visible").append("<div class='friend-messages-contenitor'><div class='friend-messages'><img class='dropdown' src='./img/arrow.webp'/><div class='delete-msg' style='display: none;'>Elimina messaggio</div><span class='messages-written'>Let me thank you.</span></div></div>");
+                    $(".messages:visible").append(generaMessaggio("Let me thank you."));
                     break;
                 case 2:
-                    $(".messages:visible").append("<div class='friend-messages-contenitor'><div class='friend-messages'><img class='dropdown' src='./img/arrow.webp'/><div class='delete-msg' style='display: none;'>Elimina messaggio</div><span class='messages-written'>Ultimate demon technique, ultimate skill...MACROOOOOOOCOOSSSSSM!!!!!!</span></div></div>");
+                    $(".messages:visible").append(generaMessaggio("Ultimate demon technique, ultimate skill...MACROOOOOOOCOOSSSSSM!!!!!!"));
                     break;
             }
         } else if(numeroChat == 9){
             switch(numeroRisposta){
                 case 1: 
-                    $(".messages:visible").append("<div class='friend-messages-contenitor'><div class='friend-messages'><img class='dropdown' src='./img/arrow.webp'/><div class='delete-msg' style='display: none;'>Elimina messaggio</div><span class='messages-written'>Se è la disperazione che cerchi..allora ti aiuterò a cercarla io! </span></div></div>");
+                    $(".messages:visible").append(generaMessaggio("Se è la disperazione che cerchi..allora ti aiuterò a cercarla io!"));
                     break;
                 case 2:
-                    $(".messages:visible").append("<div class='friend-messages-contenitor'><div class='friend-messages'><img class='dropdown' src='./img/arrow.webp'/><div class='delete-msg' style='display: none;'>Elimina messaggio</div><span class='messages-written'>Felice di vederti, Cloud</span></div></div>");
+                    $(".messages:visible").append(generaMessaggio("Felice di vederti, Cloud"));
                     break;
             }
         } else if(numeroChat == 10){
             switch(numeroRisposta){
                 case 1:
-                    $(".messages:visible").append("<div class='friend-messages-contenitor'><div class='friend-messages'><img class='dropdown' src='./img/arrow.webp'/><div class='delete-msg' style='display: none;'>Elimina messaggio</div><span class='messages-written'>La tua Luce...non perderla di vista. </span></div></div>");
+                    $(".messages:visible").append(generaMessaggio("La tua Luce...non perderla di vista."));
                     break;
                 case 2:
-                    $(".messages:visible").append("<div class='friend-messages-contenitor'><div class='friend-messages'><img class='dropdown' src='./img/arrow.webp'/><div class='delete-msg' style='display: none;'>Elimina messaggio</div><span class='messages-written'>Sephiroth. Tell me if you see him.</span></div></div>");
+                    $(".messages:visible").append(generaMessaggio("Sephiroth. Tell me if you see him."));
                     break;
             }
         } else if(numeroChat == 11) {
             switch(numeroRisposta){
                 case 1:
-                    $(".messages:visible").append("<div class='friend-messages-contenitor'><div class='friend-messages'><img class='dropdown' src='./img/arrow.webp'/><div class='delete-msg' style='display: none;'>Elimina messaggio</div><span class='messages-written'>MALA TEMPORA CURRUNT!</span></div></div>");
+                    $(".messages:visible").append(generaMessaggio("MALA TEMPORA CURRUNT!"));
                     break;
                 case 2:
-                    $(".messages:visible").append("<div class='friend-messages-contenitor'><div class='friend-messages'><img class='dropdown' src='./img/arrow.webp'/><div class='delete-msg' style='display: none;'>Elimina messaggio</div><span class='messages-written'>Principessaaaaaa!!!</span></div></div>");
+                    $(".messages:visible").append(generaMessaggio("Principessaaaaaa!!!"));
                     break;
             }
         } else if(numeroChat == 12){
             switch(numeroRisposta){
                 case 1:
-                    $(".messages:visible").append("<div class='friend-messages-contenitor'><div class='friend-messages'><img class='dropdown' src='./img/arrow.webp'/><div class='delete-msg' style='display: none;'>Elimina messaggio</div><span class='messages-written'>Vogliamo c-credere che l'avere il dono della v-vita in q-questo mondo, non s-sia un e-errore!</span></div></div>");
+                    $(".messages:visible").append(generaMessaggio("Vogliamo c-credere che l'avere il dono della v-vita in q-questo mondo, non s-sia un e-errore!"));
                     break;
                 case 2:
-                    $(".messages:visible").append("<div class='friend-messages-contenitor'><div class='friend-messages'><img class='dropdown' src='./img/arrow.webp'/><div class='delete-msg' style='display: none;'>Elimina messaggio</div><span class='messages-written'>S-signorina?</span></div></div>");
+                    $(".messages:visible").append(generaMessaggio("S-signorina?"));
                     break;
             }
         }
+        $(".messages:visible").scrollTop($(".messages:visible").height());
     }
 
     function ricercaUtente() {
@@ -160,10 +167,20 @@
         })
 
         $("#arrow").click(function(){
-            $(".messages:visible").append('<div class="my-messages-contenitor"><div class="my-messages"><img class="dropdown" src="./img/arrow.webp"/><div class="delete-msg" style="display: none;">Elimina messaggio</div><span class="messages-written">' + $("#write").val() + '</span></div></div>');
-            $("#write").val("");
-            setTimeout(rispostaInterlocutore,1000);
+            var messaggio= $("#write").val();
+            if(messaggio.length != 0) {
+                $(".messages:visible").append('<div class="my-messages-contenitor"><div class="my-messages"><img class="dropdown" src="./img/arrow.webp"/><div class="delete-msg" style="display: none;">Elimina messaggio</div><span class="messages-written">' + $("#write").val() + '</span></div></div>');
+                $("#write").val("");
+                setTimeout(rispostaInterlocutore,1000);
+                $(".messages:visible").scrollTop($(".messages:visible").height());
+            }
         });
+
+        $("#write").keyup(function(e){
+            if(e.keyCode === 13) {
+                $("#arrow").click();
+            }
+        })
 
         $("#search").keyup(ricercaUtente);
 
@@ -174,9 +191,10 @@
         $(document).on('click', '.delete-msg', function(){
             $(this).parent().parent().remove();
         })
+
     })
 
 
-// Poter inviare il messaggio con Invio
-// scrollare la chat per far vedere l'ultimo messaggio inviato
-// Fare un controllo per non inviare messaggi vuoti
+// Poter inviare il messaggio con Invio FATTO
+// scrollare la chat per far vedere l'ultimo messaggio inviato FATTO
+// Fare un controllo per non inviare messaggi vuoti FATTO
